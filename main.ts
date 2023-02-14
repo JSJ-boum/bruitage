55cbe3c3b512642114b5cf46cbaa3db3cc7dfe30
JSJ-boum <123979554+JSJ-boum@users.noreply.github.com>
2023-02-14 13:51:40 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,19 @@
 input.onButtonPressed(Button.B, function () {
-	
+    music.playMelody("D D F - D D F - ", 130)
+    music.playMelody("D D F - D D F - ", 200)
+    music.playMelody("D D F - D D F - ", 250)
+    music.playMelody("D D F - D D F - ", 315)
+    for (let index = 0; index < 3; index++) {
+        music.playMelody("D D F - D D F - ", 400)
+    }
+    for (let index = 0; index < 3; index++) {
+        music.playMelody("D D F - D D F - ", 500)
+    }
+    for (let index = 0; index < 3; index++) {
+        music.playMelody("D D F - D D F - ", 600)
+    }
+    music.playMelody("F F E E G G G G ", 500)
+    music.stopAllSounds()
 })
 basic.forever(function () {
 	
