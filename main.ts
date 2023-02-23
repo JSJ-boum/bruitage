9e7f72d3d04561dc1812f5a8ed18fc5ba45e6e73
JSJ-boum <123979554+JSJ-boum@users.noreply.github.com>
2023-02-23 11:21:11 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 input.onLogoEvent(TouchButtonEvent.Touched, function () {
-    for (let index = 0; index < 22; index++) {
+    for (let index = 0; index < 19; index++) {
         basic.showLeds(`
             . # . # .
             # # # # #
@@ -28,13 +28,13 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     music.playMelody("D D F - D D F - ", 200)
     music.playMelody("D D F - D D F - ", 250)
     music.playMelody("D D F - D D F - ", 315)
-    for (let index = 0; index < 3; index++) {
+    for (let index = 0; index < 2; index++) {
         music.playMelody("D D F - D D F - ", 400)
     }
-    for (let index = 0; index < 3; index++) {
+    for (let index = 0; index < 2; index++) {
         music.playMelody("D D F - D D F - ", 500)
     }
-    for (let index = 0; index < 3; index++) {
+    for (let index = 0; index < 2; index++) {
         music.playMelody("D D F - D D F - ", 600)
     }
     music.playMelody("F F E E G G G G ", 500)
