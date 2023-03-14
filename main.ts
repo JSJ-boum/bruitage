5a75fbbb2d5c1b8763b82593f190c5df0d23087c
JSJ-boum <123979554+JSJ-boum@users.noreply.github.com>
2023-03-14 10:01:35 -0400

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+let go_nage = 0
+radio.onReceivedNumber(function (receivedNumber) {
+    pins.analogSetPeriod(AnalogPin.P0, 10)
+    basic.pause(500)
+    pins.analogSetPeriod(AnalogPin.P0, 160)
+    basic.pause(500)
+})
 input.onLogoEvent(TouchButtonEvent.Touched, function () {
     for (let index = 0; index < 19; index++) {
         basic.showLeds(`
@@ -23,6 +30,12 @@ input.onLogoEvent(TouchButtonEvent.Touched, function () {
         . . . . .
         `)
 })
+input.onButtonPressed(Button.A, function () {
+    go_nage = 1
+})
+input.onButtonPressed(Button.B, function () {
+    go_nage = 0
+})
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     music.playMelody("D D F - D D F - ", 130)
     music.playMelody("D D F - D D F - ", 200)
@@ -41,5 +54,10 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     music.stopAllSounds()
 })
 basic.forever(function () {
-	
+    if (go_nage == 1) {
+        servos.P0.setAngle(10)
+        basic.pause(500)
+        servos.P0.setAngle(160)
+        basic.pause(500)
+    }
 })
